9b9db6d7486bb7aec76a897d4c837502160e404c
Sasha Yee <yee.sasha@abc.net.au>
2022-12-16 10:28:26 +1100

    Minor improvements to keymap
    - Added Space Cadet parenthesis
    - removed duplicated layer / fn key
    - added tap / hold for thumb mods
        - LGUI & backspace
        - Ctrl + Alt & Enter
    - Added todos for what should be done with the encoder
    - Added [ and ] keys to Layer 1.

--- keyboards/keychron/q10/ansi_encoder/keymaps/smypf/keymap.c
+++ keyboards/keychron/q10/ansi_encoder/keymaps/smypf/keymap.c
@@ -44,17 +44,17 @@ enum layers{
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_89(
-        KC_MPLY,  KC_ESC,          KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_F13,             KC_DEL,
-        KC_F16,   KC_GRV,          KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_F17,   KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_F18,   LCTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_F14,
-        KC_F19,   KC_LSFT,         KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
-        KC_F20,   KC_LCTL,         KC_LOPT,            KC_LCMD,  KC_SPC,  MO(MAC_FN),            LT(1, KC_SPC),              KC_RCMD,             KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_MPLY,  KC_ESC,            KC_F1,    KC_F2,            KC_F3,    KC_F4,         KC_F5,                    KC_F6,    KC_F7,    KC_F8,       KC_F9,   KC_F10,   KC_F11,   KC_F12,    KC_F13,    KC_DEL,
+        KC_F16,   KC_GRV,             KC_1,     KC_2,             KC_3,     KC_4,          KC_5,                     KC_6,     KC_7,     KC_8,        KC_9,     KC_0,  KC_MINS,   KC_EQL,  KC_BSPC,   KC_PGUP,
+        KC_F17,   KC_TAB,             KC_Q,     KC_W,             KC_E,     KC_R,          KC_T,                     KC_Y,     KC_U,     KC_I,        KC_O,     KC_P,  KC_LBRC,  KC_RBRC,  KC_BSLS,   KC_PGDN,
+        KC_F18,   LCTL_T(KC_ESC),     KC_A,     KC_S,             KC_D,     KC_F,          KC_G,                     KC_H,     KC_J,     KC_K,        KC_L,  KC_SCLN,  KC_QUOT,             KC_ENT,    KC_F14,
+        KC_F19,   SC_LSPO,            KC_Z,     KC_X,             KC_C,     KC_V,          KC_B,                     KC_B,     KC_N,     KC_M,     KC_COMM,   KC_DOT,  KC_SLSH,  SC_RSPC,    KC_UP,
+        KC_F20,   KC_LCTL,         KC_LOPT,            LGUI_T(KC_BSPC),   KC_SPC, LCA_T(KC_ENT),            LT(1, KC_SPC),            KC_RCMD,                                   KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_89(
         RGB_TOG,  _______,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU, _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   _______,  KC_LBRC,  KC_RBRC,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,          _______,            _______,
         _______,  _______,            _______,  _______,  _______,  _______,   _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,            _______,  _______,  _______,                       _______,            _______,                      _______,  _______,  _______),
@@ -76,6 +76,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,            _______,  _______,  _______,                       _______,            _______,                       _______,  _______,  _______),
 };
 
+// TODO
+// Prev / Next Track
+// Volume Up & Down
+// Ability to switch between options
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
